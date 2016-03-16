8f838b8ba002a866b287b1347825ff0fedfaf559
Anders Ramsay <andersr@gmail.com>
2016-03-16 13:12:09 -0400

    list cleanup

--- app/client/layout/MainLayout.jsx
+++ app/client/layout/MainLayout.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-// import Alert from 'react-s-alert'
 
 export const MainLayout = ({content}) =>  content()
 
- // <Alert />
  MainLayout.propTypes = {
   content: React.PropTypes.func.isRequired
 }
--- app/client/lists/List.jsx
+++ app/client/lists/List.jsx
@@ -5,34 +5,36 @@ export const List = (props) => {
 
   const
 
-  showDeleteBtn = (item) => {
-    return props.deleteItem? <DeleteBtn handleDelete={props.handleDeleteItem} {...props} item={item} /> : null ;
-  }
-  ,
-  noItems = (items) => items.length === 0
-  ,
-  noItemsMsg = (msg = "There are currently no items.") => <div className="centered block-padding"><span className="help-text">{msg}</span></div>
-  ,
-  listItems = (items) => {
-    return <ul className="item-list">
-            {
-              items.map((item, index) => {
-                const path = FlowRouter.path( "noteDetail" , {_id: item._id})
-                
-                return <li key={index} className="list-group-item">
-                        <div className="item-list-main-content">
-                           <a href={path}>{item.title}</a>
-                        </div>
-                        {showDeleteBtn(item)}
-                        </li>
-              })
-            }
-       </ul>
-  
-  }
+    showDeleteBtn = (item) => {
+      return props.deleteItem? <DeleteBtn handleDelete={props.handleDeleteItem} {...props} item={item} /> : null 
+    }
+    ,
 
-  return noItems(props.items)? noItemsMsg(props.noItemsMsg) : listItems(props.items) ;
+    noItems = (items) => items.length === 0
 
+    ,
+
+    noItemsMsg = (msg = "There are currently no items.") => <div className="centered block-padding"><span className="help-text">{msg}</span></div>
+    ,
+
+    listItems = (items) => {
+      return <ul className="item-list">
+              {
+                items.map((item, index) => {
+                  const path = FlowRouter.path( "noteDetail" , {_id: item._id})
+                  
+                  return <li key={index} className="list-group-item">
+                          <div className="item-list-main-content">
+                             <a href={path}>{item.title}</a>
+                          </div>
+                          {showDeleteBtn(item)}
+                          </li>
+                })
+              }
+            </ul>
+    }
+
+  return noItems(props.items)? noItemsMsg(props.noItemsMsg) : listItems(props.items)
 }
 
 List.propTypes = {
