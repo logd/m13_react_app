97f82b66dd25e560cbb947d56e9c4a89835c9f18
Anders Ramsay <andersr@gmail.com>
2016-03-16 15:38:21 -0400

    optionsMenu

--- app/lib/accounts.js
+++ app/lib/accounts.js
@@ -1,7 +1,6 @@
 AppLibRedirectPath = null;
 
 Accounts.onLogin(function(){
-  // console.log('entered onLogin');
   if (Meteor.isClient) {
     if (AppLibRedirectPath != null && AppLibRedirectPath != '/login') {
       FlowRouter.go(AppLibRedirectPath);
