7f3b360eea865ab02bb724b5d12c8c7ef90d76b3
Anders Ramsay <andersr@gmail.com>
2016-03-22 13:26:23 -0400

    added basic security

---
+++ app/server/security.js
@@ -0,0 +1,63 @@
+// --------------------------------------------------------------
+// Deny all client-side updates to user documents
+// http://guide.meteor.com/accounts.html#dont-use-profile
+// --------------------------------------------------------------
+Meteor.users.deny({
+  update: function() {
+    return true
+  }
+})
+
+// --------------------------------------------------------------
+// BROWSER POLICY 
+// https://atmospherejs.com/meteor/browser-policy
+// --------------------------------------------------------------
+const trusted = [
+  '*.google-analytics.com',
+  '*.googleapis.com',
+  '*.gstatic.com',
+  '*.googleusercontent.com',
+  'graph.facebook.com',
+  '*.fbcdn.net',
+  'secure.gravatar.com',
+  'i0.wp.com',
+  '*.typekit.net'
+]
+
+_.each(trusted, (origin) => BrowserPolicy.content.allowOriginForAll(origin))
+
+BrowserPolicy.content.allowInlineStyles()
+BrowserPolicy.content.allowFontDataUrl()
+
+
+if (process.env.NODE_ENV === 'development') {
+    BrowserPolicy.content.allowOriginForAll("localhost:*")
+    BrowserPolicy.content.allowConnectOrigin("ws://localhost:5000")
+    BrowserPolicy.content.allowConnectOrigin("http://localhost:5000")
+    BrowserPolicy.framing.allowAll()
+} else {
+
+  BrowserPolicy.framing.disallow()
+  // BrowserPolicy.content.disallowInlineScripts()
+  BrowserPolicy.content.disallowEval()
+}
+
+// --------------------------------------------------------------
+// DB Policies
+// Uses https://github.com/ongoworks/meteor-security/
+// --------------------------------------------------------------
+import {Notes} from '../both/Notes'
+
+Security.defineMethod("ifIsOwner", {
+  fetch: [],
+  transform: null,
+  deny: function (type, arg, userId, doc) {
+    return userId !== doc.ownerId;
+  }
+})
+
+//Must be signed in to create a note 
+Notes.permit('insert').ifLoggedIn().apply()
+
+//Must be the owner to update or remove a note 
+Notes.permit(['update', 'remove']).ifIsOwner().apply()
