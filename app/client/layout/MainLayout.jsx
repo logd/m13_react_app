a1cfce3b46ecb46a94ecd6559ef01994f3c2fb6c
Anders Ramsay <andersr@gmail.com>
2016-03-24 09:27:54 -0400

    styling tweaks

--- app/client/layout/MainLayout.jsx
+++ app/client/layout/MainLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Alert from 'react-s-alert'
 
-export const MainLayout = ({content}) =>  <div>{content()}<Alert /></div>
+export const MainLayout = ({content}) =>  <div className="app-container">{content()}<Alert /></div>
 
 MainLayout.propTypes = {
   content: React.PropTypes.func.isRequired
