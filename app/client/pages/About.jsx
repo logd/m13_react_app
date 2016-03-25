22c60c066624145bd4192fb34752bf080f1b059f
Anders Ramsay <andersr@gmail.com>
2016-03-25 14:47:34 -0400

    basic about page

--- app/client/pages/About.jsx
+++ app/client/pages/About.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import {AppHeader} from '../layout/AppHeader.jsx'
+import {PageTitle} from '../content/PageTitle.jsx'
 import {AppFooter} from '../layout/AppFooter.jsx'
 
 export const About = () => {
 
-	  const
-    pageContent = <section><p>Logd is a simple app for creating notes, designed and developed by <a href="https://twitter.com/andersramsay">@andersramsay</a>.</p> <p><a href="http://www.google.com/recaptcha/mailhide/d?k=01TFRfGssWj-3PuYFClMj9sA==&c=2R2mS2abKy2gGIx8plwwQg==">Contact me</a>.</p> <p><a href="https://github.com/logd/meteor_react_app">Github repo</a>.</p></section>
+	const 
+	  pagetitle = () => <PageTitle pageTitle="About" />,
 
-    return (
-    	<div className="app-container">
-        <AppHeader/>
+    pageContent = <section><p>Logd is a simple app for creating notes, designed and developed by <a href="http://andersramsay.com/">Anders Ramsay</a>.</p> <p><a href="http://www.google.com/recaptcha/mailhide/d?k=01rjxPtv-5M2bSrszeGK61Eg==&c=-jgul08jNZgfymfgqW7dlQ==">Contact Anders</a>.</p> <p><a href="https://github.com/logd/m13_react_app">Github repo</a>.</p></section>
+
+    return <div className="app-container">
+        <AppHeader headerCenter={pagetitle()} />
         <div className="main-content">
           {pageContent}
         </div>
          <AppFooter/>
       </div>
-    )
 }
 
 
